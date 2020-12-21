27582364a917b3e484b1d56b4bddd62ffc7ebe04
Romario Alves <Romariogn10@gmail.com>
2020-12-21 17:11:46 -0300

    map e filter funcionamento

---
+++ Map-filter-reduce/map.js
@@ -0,0 +1,46 @@
+const pets = [
+        {
+            name: 'rex',
+            type: 'dog',
+            age: 10,
+            weight: 30
+        },
+        {
+            name: 'miau',
+            type: 'cat',
+            age: 2,
+            weight: 2
+        },
+        {
+            name: 'gulp',
+            type: 'fish',
+            age: 1,
+            weight: 5
+        },
+        {
+            name: 'caramelo',
+            type: 'dog',
+            age: 8,
+            weight: 1
+        },
+        {
+            name: 'pitico',
+            type: 'bird',
+            age: 1,
+            weight: 0.01
+        },
+        {
+            name: 'dourado',
+            type: 'fish',
+            age: 2,
+            weight: 0.01
+        }
+]
+
+const petNames = pets.map((pet) => {
+    return pet.name
+})
+
+console.log(pets)
+console.log(petNames)
+
